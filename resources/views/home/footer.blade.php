6ccc6d952933a00ea7589341e613b4383d5ee4a8
muhennedsiffu <muhanadsoffo2002@gmail.com>
2023-05-01 17:54:45 +0300

    User page implementation

---
+++ resources/views/home/footer.blade.php
@@ -0,0 +1,21 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="inner-content">
+                    <p>Copyright © 2020 Company Name - Template by: <a href="https://www.phpjabbers.com/">PHPJabbers.com</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+<!-- Bootstrap core JavaScript -->
+<script src="{{asset('vendor')}}/jquery/jquery.min.js"></script>
+<script src="{{asset('vendor')}}/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+<!-- Additional Scripts -->
+<script src="{{asset('assets')}}/js/custom.js"></script>
+<script src="{{asset('assets')}}/js/owl.js"></script>
